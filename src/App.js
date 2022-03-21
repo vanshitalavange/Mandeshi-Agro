165af894618672aac068bb2803b7af8246e91f86
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-03-21 21:03:16 +0530

    feat: added routes for all pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import react from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home, Auth, Cart, Products, Wishlist } from "./pages/index";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Routes>
     </div>
   );
 }
