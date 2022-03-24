f5c1bc1cb8cc8efcd4140dcebfa757269c18e709
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-03-24 12:50:29 +0530

    add list of products

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,84 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    productName: "Lemon",
+    price: "₹10",
+    prevPrice: "₹20",
+    imgSrc: "assets/products/lemon.png",
+    inStock:true,
+    bestDeal:true,
+    ratingCount:13,
+    quantityCount:1
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    productName: "Apple",
+    price: "₹25",
+    prevPrice: "₹30",
+    imgSrc: "assets/products/apple.png",
+    inStock:true,
+    bestDeal:true,
+    ratingCount:15
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    productName: "Chillies",
+    price: "₹30",
+    prevPrice: "₹35",
+    imgSrc: "assets/products/chilli.png",
+    inStock:true,
+    bestDeal:true,
+    ratingCount:11
   },
+  {
+    _id: uuid(),
+    productName: "Beetroot",
+    price: "₹15",
+    prevPrice: "₹20",
+    imgSrc: "assets/products/beetroot.png",
+    inStock:true,
+    bestDeal:false,
+    ratingCount:6
+  },
+  {
+    _id: uuid(),
+    productName: "Banana",
+    price: "₹30",
+    prevPrice: "₹40",
+    imgSrc: "assets/products/banana.png",
+    inStock:true,
+    bestDeal:false,
+    ratingCount:8
+  },
+  {
+    _id: uuid(),
+    productName: "Danedar Ghee",
+    price: "₹620",
+    prevPrice: "₹650",
+    imgSrc: "assets/products/danedar-ghee.png",
+    inStock:true,
+    bestDeal:false,
+    ratingCount:17
+  },
+  {
+    _id: uuid(),
+    productName: "Green peas",
+    price: "₹40",
+    prevPrice: "₹50",
+    imgSrc: "assets/products/green-peas.png",
+    inStock:true,
+    bestDeal:false,
+    ratingCount:15
+  },
+  {
+    _id: uuid(),
+    productName: "A2 Pure Desi Ghee",
+    price: "₹1575",
+    prevPrice: "₹1685",
+    imgSrc: "assets/products/A2-ghee.png",
+    inStock:true,
+    bestDeal:true,
+    ratingCount:19
+  }
 ];
+
