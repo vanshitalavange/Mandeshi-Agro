125071f2ee7da973f92c702f29999e5af1f6fcfd
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-03-25 18:46:19 +0530

    feat: made quantity box on product card functional

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,18 +1,58 @@
-export const ProductCard = (prop) => {
+import "./ProductCard.css";
+import { useProductQuantity } from "../../contexts/product-quantity-context";
+
+export const ProductCard = ({ product }) => {
+  const { updatedProducts, setUpdatedProducts } = useProductQuantity();
+
+  /** functions to increment and decrement quantity while adding products to cart **/
+  const incrementQuantity = (productId) => {
+    const productsWithUpdatedQuantity = updatedProducts.map((item) => {
+      if (item._id === productId) {
+        item.userSetQuantity = item.userSetQuantity + 1;
+      }
+      return item;
+    });
+
+    setUpdatedProducts(productsWithUpdatedQuantity);
+  };
+  const decrementQuantity = (productId) => {
+    const productsWithUpdatedQuantity = updatedProducts.map((item) => {
+      if (item._id === productId) {
+        item.userSetQuantity =
+          item.userSetQuantity !== 1
+            ? item.userSetQuantity - 1
+            : item.userSetQuantity;
+      }
+      return item;
+    });
+    setUpdatedProducts(productsWithUpdatedQuantity);
+  };
+
+  /**  product prop **/
+  const {
+    _id,
+    productName,
+    price,
+    prevPrice,
+    imgSrc,
+    ratingCount,
+    userSetQuantity,
+  } = { ...product };
+
   return (
     <div class="product-card flex-column">
       <div class="product-img-container">
-        <img class="responsive-img" src={prop.product.imgSrc} alt={prop.product.productName} />
+        <img class="responsive-img" src={imgSrc} alt={productName} />
       </div>
       <span class="wishlist">
         <i class="fa fa-heart"></i>
       </span>
       <div class="product-card-body flex-column">
-        <h3 class="product-title">{prop.product.productName}</h3>
+        <h3 class="product-title">{productName}</h3>
         <div class="price-rating-wrapper flex-row justify-space-between">
           <div class="product-price-container flex-row">
-            <h4 class="price">{prop.product.price}</h4>
-            <h5 class="price-line-through align-end">{prop.product.prevPrice}</h5>
+            <h4 class="price">{price}</h4>
+            <h5 class="price-line-through align-end">{prevPrice}</h5>
           </div>
           <div class="card-rating flex-row">
             <div class="flex-row align-center">
@@ -22,18 +62,13 @@ export const ProductCard = (prop) => {
               <span class="material-icons rating-icon">star_half</span>
               <span class="material-icons rating-icon fs-16">star_border</span>
             </div>
-            <p class="rating-count align-center">{prop.product.ratingCount}</p>
+            <p class="rating-count align-center">{ratingCount}</p>
           </div>
         </div>
         <div class="product-card-actions flex-row flex-wrap">
           <div class="quantity-box flex-row">
             <button
-              onClick={() =>
-                // setQuantityCount(
-                //   quantityCount <= 1 ? quantityCount : quantityCount - 1
-                // )
-                console.log("")
-              }
+              onClick={() => decrementQuantity(_id)}
               class="btn-decrement"
             >
               -
@@ -42,15 +77,11 @@ export const ProductCard = (prop) => {
               class="product-quantity"
               type="number"
               name="quantity"
-              value={1}
+              value={userSetQuantity}
               min="1"
             />
             <button
-              onClick={() =>
-                // setQuantityCount((quantityCount) => quantityCount + 1)
-                console.log("")
-
-              }
+              onClick={() => incrementQuantity(_id)}
               class="btn-increment"
             >
               +
