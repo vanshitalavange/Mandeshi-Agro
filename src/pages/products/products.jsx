27ba4be6a1f03e3aeff6ba18ce3a876676f646ab
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-03-25 21:06:26 +0530

    displayed all products

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,3 +1,40 @@
+import "./products.css";
+import { Filters } from "../../components/filters/Filters";
+import { useProductQuantity } from "../../contexts/product-quantity-context";
+import { ProductCard } from "../../components/productCard/ProductCard";
 export function Products() {
-  return <div>This is the Products page</div>;
+  const { updatedProducts, setUpdatedProducts } = useProductQuantity();
+  return (
+    <div class="page-main">
+      <Filters />
+      <section class="products">
+{updatedProducts.map(
+        ({
+          _id,
+          productName,
+          price,
+          prevPrice,
+          imgSrc,
+          bestDeal,
+          ratingCount,
+          userSetQuantity,
+        }) => {
+          return (
+            <ProductCard
+              product={{
+                _id,
+                productName,
+                price,
+                prevPrice,
+                imgSrc,
+                bestDeal,
+                ratingCount,
+                userSetQuantity,
+              }}
+            />
+          );
+        }
+      )}</section>
+    </div>
+  );
 }
