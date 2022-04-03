b0dcf0f32a3e6acebd285653aec926774a54ac7a
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-04-03 13:46:01 +0530

    fix: passed correct arguments to set state

--- src/services/auth-services/login-service.js
+++ src/services/auth-services/login-service.js
@@ -4,8 +4,8 @@ export const loginHandler = async (loginCredentials,setUserState,navigate,setErr
         const response = await axios.post('/api/auth/login', loginCredentials)
         if (response.status === 200) {
             localStorage.setItem('authToken', response.data.encodedToken)
-            const { firstName, cart, wishlist } = response.data.foundUser
-            setUserState({ loginStatus: true, userDetails: { firstName, cart, wishlist } })
+            const { firstName } = response.data.foundUser
+            setUserState({ loginStatus: true,firstName })
             navigate("/")
         }
     } catch (error) {
