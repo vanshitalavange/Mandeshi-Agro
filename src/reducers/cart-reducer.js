aff6df116d1ac9617dffecd69f951b557c23a278
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-04-04 00:58:11 +0530

    add cart reducer and cart context

--- src/reducers/cart-reducer.js
+++ src/reducers/cart-reducer.js
@@ -1,8 +1,9 @@
 export const cartReducer = (state, { type, payload }) => {
     switch (type) {
         case "ADD_ITEM_TO_CART":
-        case "REMOVE_FROM_CART":
+        case "REMOVE_ITEM_FROM_CART":
         case "GET_CART":
+        case "UPDATE_CART":
             return [...payload]
         default: return state
     }
